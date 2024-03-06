78760faa7d231aeba84badcf82be11bdccb55a5d
M Ali Zahid <aliz0908@hotmail.com>
2024-03-06 23:35:49 +0500

    Redesigning Project

---
+++ src/components/CV.jsx
@@ -0,0 +1,16 @@
+import React from 'react';<div className=""></div>
+// import Header from './Header';
+// import Summary from './Summary';
+// import WorkExperience from './WorkExperience';
+// import Education from './Education';
+// import SkillsCertifications from './SkillsCertifications';
+
+function CV() {
+  return (
+    <div className="CV">
+        
+    </div>
+  );
+}
+
+export default CV;
